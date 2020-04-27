bbf3dbf4869307e9e9da5709e9bad6c033b4f638
Jdaithanawong <47125467+Jdaithanawong@users.noreply.github.com>
2020-04-28 04:17:11 +0700

    Add files via upload

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -6,6 +6,7 @@ public class ArrayQueue<T> implements Queue<T>{
 	private T[] Queue;
 	private static int front, rear;
 	
+	//constructor. initialize array of queue with length 10 and set front and rear to 0 because the array is empty at first.
 	public ArrayQueue()
 	{
 		front = rear = 0;
@@ -14,6 +15,8 @@ public class ArrayQueue<T> implements Queue<T>{
 	    
 	      
 	}
+	
+	//removes item from queue
 	public T dequeue() throws Exception
 	{
 		T placeholder = (T) Queue[front];
@@ -23,6 +26,7 @@ public class ArrayQueue<T> implements Queue<T>{
 		
 	}
 	
+	//inserts item taken as parameter into queue while doubling size if queue array happens to be full
 	public void enqueue(T item)
 	{
 		 if (size == Queue.length) 
@@ -30,10 +34,11 @@ public class ArrayQueue<T> implements Queue<T>{
 			 doubleSize();
 	     }
 		 Queue[rear] = item; 
-         rear++;
+         rear++; //after adding item rear index is increased by 1. same goes for size.
          size++;
 	}
 	
+	//returns true if empty and false otherwise
 	public boolean empty()
 	{
 		if(size == 10)
@@ -46,6 +51,7 @@ public class ArrayQueue<T> implements Queue<T>{
 		}
 	}
 	
+	//method for doubling the size of queue array when it is full. is called in enqueue.
 	public void doubleSize()
 	{
 		 int newSize = Queue.length * 2;
--- ArrayStack.java
+++ ArrayStack.java
@@ -5,12 +5,15 @@ public class ArrayStack<T> implements Stack<T>{
 	protected int size;
 	private T[] Stack;
 	
+	//constructor
 	public ArrayStack()
 	{
 		 Stack = (T[]) new Object[10];
 	     size = 0;
 	}
-
+	
+	
+	//inserts item into the stack array and also doubles the length of the array if it happens to be full.
 	public void push(T item)
 	{
 		 if (size == Stack.length) 
@@ -19,18 +22,24 @@ public class ArrayStack<T> implements Stack<T>{
 	     }
 		Stack[size++] = item;
 	}
+	
+	//returns item at the top of the stack array while also removing it
 	public T pop() throws Exception
 	{
         T popped = (T) Stack[--size];
         Stack[size] = null;
         return popped ;
 	}
+	
+	//returns the item at the top of the stack array without removing it
 	public T peek() throws Exception
 	{
 		T peeked = (T) Stack[size];
 		return peeked;
 	}
- 	public boolean empty()
+	
+	//returns empty if size is 0 and false if it is any other value.
+ 	public boolean empty() 
 	{
 		if(size == 0)
 		{
@@ -42,6 +51,7 @@ public class ArrayStack<T> implements Stack<T>{
 		}
 	}
 
+ 	//function that doubles the size of the object array stack. called when the array is full.
  	public void doubleSize()
 	{
  		int newSize = Stack.length * 2;
